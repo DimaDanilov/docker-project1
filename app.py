39992c13bee410cc92a738999373cc46e58f1b15
DimaDanilov <dima.danilov.2012@mail.ru>
2021-04-02 21:20:53 +0300

    added new requests

--- app.py
+++ app.py
@@ -18,6 +18,15 @@ def get_hit_count():
             time.sleep(0.5)
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
     count = get_hit_count()
     return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
